d3ced2bf479b07f36cd4d825830ec2329c202a95
Mauricio <mau.movilla@gamil.com>
2016-08-20 01:50:05 -0500

    Start

---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+	GameObject position;
+
+	[Range(0f,1f)] public float Smoothness = 0.2f;
+	// Use this for initialization
+	void Start () {
+		position = GameObject.FindGameObjectWithTag("PlayerCameraPosition");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position,position.transform.position,Smoothness*Time.deltaTime);
+		transform.rotation = Quaternion.Lerp(transform.rotation,position.transform.rotation,Smoothness*Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonMovement : MonoBehaviour {
+	public float Speed = 1f;
+	CharacterController Controller;
+	float vertical;
+	float horizontal;
+	Vector3 moveDirection;
+	// Use this for initialization
+	void Start () {
+		Controller = GetComponent<CharacterController>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		moveDirection = Vector3.zero;
+		vertical = Input.GetAxisRaw("Vertical");
+		horizontal = Input.GetAxisRaw("Horizontal");
+		moveDirection.z = vertical;
+		moveDirection = transform.TransformDirection(moveDirection);
+		transform.Rotate(0f,horizontal*Speed*25*Time.deltaTime,0f);
+		Controller.Move(moveDirection*Speed*Time.deltaTime);
+	}
+}
